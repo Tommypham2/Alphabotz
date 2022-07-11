45f35259d99f79b70f089c81ab5715eef61b816b
Tommy Pham <tommy16pham@gmail.com>
2022-07-11 16:09:38 -0500

    Creation

---
+++ commands/play.js
@@ -0,0 +1,34 @@
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
+module.exports = {
+    name: 'play',
+    description: 'Joins and plays video from youtube',
+    execute(message, args) {
+        const voiceChannel = message.member.voice.channel;
+
+        if(!voiceChannel) return message.channel.send('Join a channel to play a song');
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if(!permissions.has('CONNECT')) return message.channel.send('You dont have permission to use this function');
+        if(!permissions.has('SPEAK')) return message.channel.send('You dont have permission to use this function');
+        if(!args.length) return message.channel.send('You need to send a second argument');
+
+        const connection = await voiceChannel.join();
+
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+
+            return (videoResult.videos.length > 1)? videoResult.videos[0] : null;
+        }
+
+        const video = await videoFinder(args.join(' '));
+
+        if(video){
+            const stream = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+            })
+        }
+    }
+}
